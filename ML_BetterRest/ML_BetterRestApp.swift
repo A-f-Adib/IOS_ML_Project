f430e83965029310d77d51bff0a571cd656711b5
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-09-28 20:32:22 +0600

    Initial Commit

---
+++ ML_BetterRest/ML_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_BetterRestApp.swift
+//  ML_BetterRest
+//
+//  Created by A.f. Adib on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ML_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
